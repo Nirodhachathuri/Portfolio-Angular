d78ad64a618f25f778b193bfc07618b208b20906
Nirodhachathuri <chathuri.malalagama@ymail.com>
2023-10-22 14:59:00 +0530

    changes

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -14,8 +14,15 @@ export class HomeComponent implements OnInit {
     // ['Address','326, Wilgoda Road, Kurunegala, Sri Lanka']
   ];
   aboutMe:string[]=[
-    "Experienced Software Engineer with 3 years of hands-on expertise, equipped with technical abilities and a decisive nature necessary for successful job development. Expert in gathering requirements, designing systems, and optimizing code. Focused and adaptable professional with a proven track record of 3 years in software engineering positions, skilled at flexing to various roles to meet company and customer needs."
-  ]
+    'Experienced Software Engineer with 3 years of hands-on expertise, equipped with technical abilities and a decisive nature necessary for successful job development. Expert in gathering requirements, designing systems, and optimizing code. Focused and adaptable professional with a proven track record of 3 years in software engineering positions, skilled at flexing to various roles to meet company and customer needs.',
+    'I am seeking challenging opportunities in the field of Computer Science. What sets me apart is my strong research background, my eagerness to learn new technologies, and my enthusiasm for seeking inspiration and ideas from experienced professionals. This is what makes me unique.',
+    'Why I Am Special',
+    '  - I did in my Msc research Natural Language Processing in Youtube Videos, ',
+    '  - I did emotional Recognition project in Computer Vision related project',
+    '  - As a part time I am doing freelancing Projects',
+    '  - I am working as a software engineer in Full time worker'
+  ]  
+  
   constructor() { }
 
   ngOnInit(): void {
